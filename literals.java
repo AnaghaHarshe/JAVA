97d7673b4d5c4f0a3bb605187b7a27e2b54f5435
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-07-25 22:48:54 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,16 @@
+public class literals
+{
+    public static void main(String[] args)
+    {
+        byte age=34;
+        int age2=14;
+        short age3=555;
+        long agedion=123647890;
+        char ch='a';
+        double d1=4.66;
+        boolean a=true;
+        System.out.println(age3);
+        String str="Anagha";
+        System.out.println(str);
+    }
+}

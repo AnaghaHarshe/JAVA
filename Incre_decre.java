a777c6ebe2738682d1557a8927c61a46b1bd0070
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-07-29 19:08:07 +0530

    Add files via upload

---
+++ Incre_decre.java
@@ -0,0 +1,13 @@
+//Increment and Decrement
+class Incre_decre
+{
+    public static void main (String[] args)
+    {
+        int a=56;
+
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+    }
+}

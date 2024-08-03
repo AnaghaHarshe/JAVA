7bb4e9aab70c9217f7b69d3205a8fef9d1096f49
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-08-03 16:27:14 +0530

    Add files via upload

---
+++ takinginput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class takinginput
+{
+   public static void main(String[] args) 
+   {
+     System.out.println("Taking Input From the User");
+     Scanner sc= new Scanner(System.in);
+//     System.out.println("enter number 1");
+//     float a= sc.nextFloat();
+// //int a=sc.nextInt();    
+//     System.out.println("Eneter number 2");
+//     float b= sc. nextFloat();
+//     float sum=a+b;
+// // int b=sc.nextInt();
+// //    int sum=a+b;
+//     System.out.println("The sum of these numbers is ");
+//     System.out.println(sum);
+    String str = sc.nextLine();
+    System.out.println(str);
+   }
+}

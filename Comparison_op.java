756ff60ae3dc99c47e54e22783f4546dd9fadef2
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-08-04 16:08:22 +0530

    Add files via upload

---
+++ Comparison_op.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Comparison_op
+{
+    public static void main(String[] args)
+    
+    {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a>8);
+        
+    }
+}

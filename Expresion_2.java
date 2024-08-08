772d7aa46057b4d0600a07c0ba4e0ab495892982
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-08-08 23:54:33 +0530

    Add files via upload

---
+++ Expresion_2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Expresion_2
+{
+     public static void main(String[] args);
+ {
+  float v=40;
+  float u=4;
+  float a=6;
+  float s=8;
+  float formula=(v*v-u*u)/(2*a*s);
+  System.out.println(formula);
+ }
+}

ae3ce45c59be913e1e64ef649cc2dceca8e84f77
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-08-11 17:13:21 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,9 @@
+/*using java  Create a class Circle that encapsulates the properties of a circle.
+The class should have a private field for the radius, a constructor to initialize the radius, and methods to calculate the area and circumference of the circle.
+NOTE: use Math.PI for PI calculations (DO NOT USE 22/7)
+ 
+Guideline to Solve:
+§ Define the Circle class with attributes and constructors.
+§ Use the this keyword in the parameterized constructor.
+§ Define a getArea() funtion that returns the area of there Circle (use Math.PI)
+§ Define a getCircumference() funtion that returns the circumference of there Circle (use Math.PI)*/
